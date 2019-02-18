b2ed0c17d4612c05d4b26cf614c6dfe4ae2e9092
naXa! <naxa@nextmail.ru>
2019-02-19 01:28:54 +0300

    add new solution: Account Test

--- src/main/java/by/naxa/testdome/MergeNames.java
+++ src/main/java/by/naxa/testdome/MergeNames.java
@@ -1,3 +1,12 @@
+//  Created by naXa! on 18/02/2019.
+//  Copyright © 2019 naXa!. All rights reserved.
+//
+/*
+ This code is a practice Java interview question from testdome.com
+ Problem statement:
+    Implement the uniqueNames method. When passed two arrays of names, it will return an array containing the names that appear in either or both arrays. The returned array should have no duplicates.
+    For example, calling MergeNames.uniqueNames(new String[]{'Ava', 'Emma', 'Olivia'}, new String[]{'Olivia', 'Sophia', 'Emma'}) should return an array containing Ava, Emma, Olivia, and Sophia in any order.
+ */
 package by.naxa.testdome;
 
 import java.util.Arrays;
