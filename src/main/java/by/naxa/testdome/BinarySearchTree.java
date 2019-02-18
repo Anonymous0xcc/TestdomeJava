2dff35144b0f80c898df76e3a9784bfc84fa589d
naXa! <naxa@nextmail.ru>
2019-02-18 23:35:40 +0300

    update BinarySearchTree: shorten code

--- src/main/java/by/naxa/testdome/BinarySearchTree.java
+++ src/main/java/by/naxa/testdome/BinarySearchTree.java
@@ -13,7 +13,7 @@
 package by.naxa.testdome;
 
 class Node {
-	//starting code left these public, which makes code a bit simpler
+    //starting code left these public, which makes code a bit simpler
     public int value;
     public Node left, right;
 
@@ -26,34 +26,23 @@ class Node {
 
 public class BinarySearchTree {
     public static boolean contains(Node root, int value) {
-        if(root.value==value){
-        	return true;
-        }
-        else if(root.value<value){
-        	//value we are searching for is greater than that held by node we are at
-        		if(root.right!=null){
-        			return contains(root.right, value);
-        		}
-        		else{
-        			return false;
-        		}
-        }
-        else{
-        	//value we are searching for is greater than that held by node we are at
-        	if(root.left!=null){
-        		return contains(root.left, value);
-        	}else{
-        		return false;
-        	}
-        	
-        }
+        if (root == null)
+            return false;
+        if (root.value == value)
+            return true;
+        if (root.value < value)
+            // value we are searching for is greater than that held by node we are at
+            return contains(root.right, value);
+        else
+            // value we are searching for is greater than that held by node we are at
+            return contains(root.left, value);
     }
-    
+
     public static void main(String[] args) {
         Node n1 = new Node(1, null, null);
         Node n3 = new Node(3, null, null);
         Node n2 = new Node(2, n1, n3);
-        
+
         System.out.println(contains(n2, 3));
     }
 }
